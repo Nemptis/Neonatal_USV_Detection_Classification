d678f637dfa0ffc4fbb462aad904987bd3996feb
Marvin Walther <mwalther@uni-bremen.de>
2024-08-29 11:34:08 +0200

    Updated mouse_dataset

--- utils.py
+++ utils.py
@@ -4,12 +4,16 @@ import os
 def get_file_list(folder:str, ext:str='.csv'):
     return sorted([os.path.join(folder, f) for f in os.listdir(os.path.join(folder)) if f.endswith(ext)])
 
+
+
 def save_csv(file_path, data, header=['starttime', 'endtime']):
     with open(file_path, 'w') as file:
         wr = csv.writer(file, delimiter=';', lineterminator='\n', quoting=0)
         wr.writerow(header)
         for call in data:
             wr.writerow(call)
+
+
 
 def parse_metadata(filename: str):
     parts = os.path.basename(filename).split('.')[0].split('_')
